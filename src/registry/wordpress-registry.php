b5a297a8aed55a6635b4e8df0e3e36bb6d3ab35f
Ryan Raub <ryan.raub@asu.edu>
2016-10-26 11:12:03 -0700

    hello world plugin with a shortcode

---
+++ src/registry/wordpress-registry.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Register any classes to be registerd that provide:
+ *  shortcodes, pages, enqueuers, etc
+ */
+
+return [
+  ASURFIWordPress\Shortcodes\ASU_RFI_Form_Shortcodes::class
+];
