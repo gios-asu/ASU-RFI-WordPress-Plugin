eea124a6a29c66821fc6ac393f370a62aa155c9b
Ryan Raub <ryan.raub@asu.edu>
2016-11-23 11:18:19 -0700

    direct call protection for student type service

--- src/services/student-type-service.php
+++ src/services/student-type-service.php
@@ -2,6 +2,13 @@
 
 namespace ASURFIWordPress\Services;
 
+// Avoid direct calls to this file
+if ( ! defined( 'ASU_RFI_WORDPRESS_PLUGIN_VERSION' ) ) {
+  header( 'Status: 403 Forbidden' );
+  header( 'HTTP/1.1 403 Forbidden' );
+  exit();
+}
+
 class StudentTypeService {
 
   public static function get_student_types() {
