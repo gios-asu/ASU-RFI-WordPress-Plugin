3ae46e4e9c52dedc95eaf397aa7b2f006ede4765
Ryan Raub <ryan.raub@asu.edu>
2016-12-08 15:03:51 -0700

    tests for test mode defaults

--- tests/unit/test-shortcodes.php
+++ tests/unit/test-shortcodes.php
@@ -26,13 +26,22 @@ class ShortCodesTest extends WP_UnitTestCase {
     $source_id_default_in_db = 999; // see data-loader.php
 
     $result = do_shortcode( '[asu-rfi-form]' );
-    $this->assertContains('value="'.$source_id_default_in_db.'"', $result);
+    $this->assertContains('name="source_id" value="'.$source_id_default_in_db.'"', $result);
   }
 
   function test_shortcode_uses_source_id_from_attribute_over_db() {
     $result = do_shortcode( '[asu-rfi-form source_id=12345]' );
-    $this->assertContains('value="12345"', $result);
+    $this->assertContains('name="source_id" value="12345"', $result);
   }
 
+  function test_shortcode_default_test_mode() {
+    $result = do_shortcode( '[asu-rfi-form]' );
+    $this->assertContains('name="testmode" value="Prod"', $result);
+  }
+  
+  function test_shortcode_default_test_mode() {
+    $result = do_shortcode( '[asu-rfi-form test_mode="test"]' );
+    $this->assertContains('name="testmode" value="Test"', $result);
+  }
 
 }
