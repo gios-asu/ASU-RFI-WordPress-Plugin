f8669f8ccc49fbe7484313e7d79c65e1a0ad981d
Ryan Raub <ryan.raub@gmail.com>
2017-03-21 16:15:55 -0700

    added another geocoding test for google’s dns server

--- tests/unit/test-client-geocoding-service.php
+++ tests/unit/test-client-geocoding-service.php
@@ -14,12 +14,22 @@ use ASURFIWordPress\Services\Client_Geocoding_Service;
  */
 class Client_Geocoding_Service_Test extends WP_UnitTestCase {
 
-  // TODO Mock the ip from and the headers
 
   function test_get_client_ip() {
     $geo_location_json_string = Client_Geocoding_Service::client_geo_location();
     $this->assertInternalType('string', $geo_location_json_string);
     $this->assertContains('country', $geo_location_json_string);
   }
+
+  function test_get_client_ip_with_external_ip() {
+    function apache_request_headers() {
+      return array('HTTP_CLIENT_IP' => '8.8.8.8'); // This is google's DNS Server in California
+    }
+    $geo_location_json_string = Client_Geocoding_Service::client_geo_location();
+    $this->assertInternalType('string', $geo_location_json_string);
+    $this->assertContains('"country_code":"US"', $geo_location_json_string);
+    $this->assertContains('"zip_code":"94035"', $geo_location_json_string);
+  }
+
 
 }
