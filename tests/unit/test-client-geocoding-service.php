ad1135cdc1e6643cb615727f5fb52013015ad4f3
Walter McConnell <Walter.McConnell@asu.edu>
2019-05-22 16:50:39 -0700

    Update GeoLocation Test
    Our original test for the gelocation service was failing because the new api endpoint was not returning a ZIP code for the IP '8.8.8.8'
    
    I switched the IP address to the one used in IPStack's documentation, and used the ZIP code also shown in the docs.

--- tests/unit/test-client-geocoding-service.php
+++ tests/unit/test-client-geocoding-service.php
@@ -23,12 +23,14 @@ class Client_Geocoding_Service_Test extends WP_UnitTestCase {
 
   function test_get_client_ip_with_external_ip() {
     function apache_request_headers() {
-      return array('HTTP_CLIENT_IP' => '8.8.8.8'); // This is google's DNS Server in California
+      // Using IP Address, and corresponding results, from IPStack's own documentation,
+      // as the original 8.8.8.8 was no longer providing a ZIP code
+      return array('HTTP_CLIENT_IP' => '134.201.250.155');
     }
     $geo_location_json_string = Client_Geocoding_Service::client_geo_location();
     $this->assertInternalType('string', $geo_location_json_string);
     $this->assertContains('"country_code":"US"', $geo_location_json_string);
-    $this->assertContains('"zip_code":"94035"', $geo_location_json_string);
+    $this->assertContains('"zip_code":"91773"', $geo_location_json_string);
   }
 
 
